703631d1d281916d1f573290b1b8745cddd49b2d
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-25 21:16:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+ class School
+ 
+ attr_reader :name
+ attr_accessor :roster
+ 
+ def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster 
+    @roster
+  end
+
+  def add_student(name, grade)
+    if roster.has_key?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = [name]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  
+end
